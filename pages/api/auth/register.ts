7ea7fa388fb1f995dda57acb6e1a40041c1563fe
zuluu <118282162+Prolific-African-Community@users.noreply.github.com>
2026-06-10 11:18:22 +0200

    Remove sensitive database logging

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,4 +1,3 @@
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { UserRole } from '@prisma/client';
 import { prisma } from '../../../lib/prisma';
